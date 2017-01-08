9d0b3ef516e526ba379c386a782ae93149c783e6
Alex Wendland <me@alexwendland.com>
2017-01-08 15:50:58 -0500

    Basic upload functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const multer = require('multer')
+
+const port = process.env.PORT || 3000
+const uploadDir = process.env.UPLOAD_DIR || (() => { throw new Error('Must define an upload directory') })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+const uploading = multer({
+  'dest': uploadDir,
+  'limits': { 'fileSize': 2e7 },
+})
+
+app.post('/', uploading.array('ebooks'), (req, res) => {
+  console.log(`Uploaded ${req.files.length} files: ${(req.files || []).map(f => f.originalname)}`);
+  res.status(204).end();
+});
+
+app.listen(3000, () => {
+  console.log(`ePub uploader listening on port ${port}`)
+});
